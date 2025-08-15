9984bcd00b8aef47288a1ec1e617095ac9b4a1dd
NazarPetrakov <nazarpetrakov1@gmail.com>
2025-08-15 16:11:11 +0300

    add Common class library - packed Common to Common.1.0.1.nupkg

---
+++ src/BuildingBlocks/Common/Entities/BaseEntity.cs
@@ -0,0 +1,8 @@
+namespace Common.Entities;
+
+public class BaseEntity
+{
+    public Guid Id { get; set; }
+    public DateTimeOffset CreatedAt { get; set; } = DateTimeOffset.UtcNow;
+    public DateTimeOffset? UpdatedAt { get; set; }
+}
