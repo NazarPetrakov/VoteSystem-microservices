a81bf487c55eef7004987dac235498f70efd6dc3
NazarPetrakov <nazarpetrakov1@gmail.com>
2025-09-09 14:04:41 +0300

    v1.0.12 - add new vote and poll error

--- src/BuildingBlocks/Common/Errors/PollErrors.cs
+++ src/BuildingBlocks/Common/Errors/PollErrors.cs
@@ -4,12 +4,13 @@ namespace Common.Errors;
 
 public static class PollErrors
 {
-    public static Error NotFound(Guid id) => new("Polls.NotFound", $"The poll with Id '{id}' not found");
+    public static Error NotFound(Guid id) => new("Polls.NotFound", $"The poll with Id '{id}' not found.");
     public static Error MissingOption(Guid pollId, Guid pollOptionId) => new("Polls.MissingOption",
         $"The poll with Id '{pollId}' does not contain the option with Id '{pollOptionId}'.");
+    public static Error AlreadyClosed(Guid pollId) => new("Polls.AlreadyClosed", $"The poll with Id '{pollId}' is already closed.");
 }
 public static class PollOptionErrors
 {
     public static Error NotFound(Guid id) => new("PollOptions.NotFound",
-        $"The poll option with Id '{id}' not found");
+        $"The poll option with Id '{id}' not found.");
 }
--- src/BuildingBlocks/Common/Errors/VoteErrors.cs
+++ src/BuildingBlocks/Common/Errors/VoteErrors.cs
@@ -4,5 +4,6 @@ namespace Common.Errors;
 
 public static class VoteErrors
 {
-    public static Error NotFound(Guid id) => new("Votes.NotFound", $"The vote with Id '{id}' not found");
+    public static Error NotFound(Guid id) => new("Votes.NotFound", $"The vote with Id '{id}' not found.");
+    public static Error ClosedPoll(Guid pollId) => new("Votes.ClosedPoll", $"The poll with Id '{pollId}' is closed.");
 }
