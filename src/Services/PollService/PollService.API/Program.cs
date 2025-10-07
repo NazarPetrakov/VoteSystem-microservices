5c4464ac92c77be15e49948907f45f3c3ad9f8df
NazarPetrakov <nazarpetrakov1@gmail.com>
2025-10-07 16:26:13 +0300

    add CORS

--- src/Services/PollService/PollService.API/Program.cs
+++ src/Services/PollService/PollService.API/Program.cs
@@ -25,6 +25,10 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseCors(c =>
+{
+    c.AllowAnyHeader().AllowCredentials().AllowAnyMethod().WithOrigins("http://localhost:4200");
+});
 app.UseAuthorization();
 
 app.MapControllers();
