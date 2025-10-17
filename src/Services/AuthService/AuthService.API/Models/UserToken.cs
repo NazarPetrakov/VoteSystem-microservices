9e9b5bcf361db56a5358af3540c437550825ab9b
NazarPetrakov <nazarpetrakov1@gmail.com>
2025-10-17 16:21:57 +0300

    Add Id to UserToken

--- src/Services/AuthService/AuthService.API/Models/UserToken.cs
+++ src/Services/AuthService/AuthService.API/Models/UserToken.cs
@@ -1,7 +1,8 @@
 namespace AuthService.API.Models;
 
-public class UserToken(string userName, string token)
+public class UserToken(int userId, string userName, string token)
 {
+    public int UserId { get; set; } = userId;
     public string UserName { get; init; } = userName;
     public string Token { get; init; } = token;
 }
