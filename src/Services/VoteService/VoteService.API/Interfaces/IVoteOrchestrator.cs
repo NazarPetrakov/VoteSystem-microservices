31645c2d8c852738a01790a38408e90b505f56e6
NazarPetrakov <nazarpetrakov1@gmail.com>
2025-10-18 16:07:47 +0300

    Add GetUserVotes endpoint

--- src/Services/VoteService/VoteService.API/Interfaces/IVoteOrchestrator.cs
+++ src/Services/VoteService/VoteService.API/Interfaces/IVoteOrchestrator.cs
@@ -1,11 +1,13 @@
+using System.Linq.Expressions;
 using Common.Result;
 using VoteService.API.Contracts.Dtos;
+using VoteService.API.Models;
 
 namespace VoteService.API.Interfaces;
 
 public interface IVoteOrchestrator
 {
-    Task<Result<List<VoteResponse>>> GetAllAsync();
+    Task<Result<List<VoteResponse>>> GetAllAsync(Expression<Func<Vote, bool>>? filter = null);
     Task<Result<VoteResponse>> GetAsync(Guid voteId);
     Task<Result<VoteResponse>> CreateAsync(CreateVoteRequest createVoteRequest);
     Task<Result> DeleteAsync(Guid voteId);
