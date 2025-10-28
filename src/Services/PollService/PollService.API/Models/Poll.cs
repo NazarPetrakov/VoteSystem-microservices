ca2880bd459e48dab8d710877551c308c950b70d
NazarPetrakov <nazarpetrakov1@gmail.com>
2025-10-28 16:35:22 +0200

    Add duration to Poll entity

--- src/Services/PollService/PollService.API/Models/Poll.cs
+++ src/Services/PollService/PollService.API/Models/Poll.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations.Schema;
 using Common.Entities;
 
 namespace PollService.API.Models;
@@ -7,6 +8,16 @@ public class Poll : BaseEntity<Guid>
     public required string Title { get; set; }
     public string? Topic { get; set; }
     public bool IsClosed { get; set; }
+    public TimeSpan Duration { get; set; }
+
+    [NotMapped]
+    public DateTime EndTime => CreatedAt.UtcDateTime.Add(Duration);
+
+    [NotMapped]
+    public bool IsExpired => DateTime.UtcNow > EndTime;
+
+    [NotMapped]
+    public bool IsActive => !IsClosed && !IsExpired;
 
     public int UserId { get; set; }
     public UserCache User { get; set; } = null!;
