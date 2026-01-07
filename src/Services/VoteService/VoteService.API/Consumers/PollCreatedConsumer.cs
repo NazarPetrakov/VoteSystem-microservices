b920b86bdae30158c434e8b4cfda9436e2c3ff6e
NazarPetrakov <nazarpetrakov1@gmail.com>
2026-01-07 18:23:46 +0200

    Add creating poll options consuming PollCreated event

--- src/Services/VoteService/VoteService.API/Consumers/PollCreatedConsumer.cs
+++ src/Services/VoteService/VoteService.API/Consumers/PollCreatedConsumer.cs
@@ -6,17 +6,32 @@ using VoteService.API.Models;
 namespace VoteService.API.Consumers;
 
 public class PollCreatedConsumer(ILogger<PollCreatedConsumer> logger,
-    IRepository<PollCache, Guid> pollRepository) : IConsumer<PollCreated>
+    IRepository<PollCache, Guid> pollRepository,
+    IRepository<PollOptionCache, Guid> pollOptionRepository) : IConsumer<PollCreated>
 {
     public async Task Consume(ConsumeContext<PollCreated> context)
     {
         var pollCreated = context.Message;
 
-        await pollRepository.CreateAndSaveAsync(new PollCache
+        var createdPoll = await pollRepository.CreateAndSaveAsync(new PollCache
         {
             Id = pollCreated.PollId,
             IsClosed = pollCreated.IsClosed
         });
+
+        if (createdPoll.Id != pollCreated.PollId)
+        {
+            logger.LogWarning("Poll with id - {PollId} was not created", pollCreated.PollId);
+        }
+
+        foreach (var optionId in pollCreated.OptionIds)
+        {
+            await pollOptionRepository.CreateAndSaveAsync(new PollOptionCache()
+            {
+                Id = optionId,
+                PollId = createdPoll.Id
+            });
+        }
 
         logger.LogInformation($"{typeof(PollCreatedConsumer)}: poll id - {pollCreated.PollId}");
     }
